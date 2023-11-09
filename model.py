8b31892bf8108221a18b24814a7b13e1dab8be7e
sihyeon <sihyun060800@gmail.com>
2023-11-09 17:16:00 +0900

    PositionalEncoding

--- model.py
+++ model.py
@@ -3,16 +3,15 @@ import torch.nn as nn
 import math
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, dim_model, max_len, device):
+    def __init__(self, dim_model, max_len):
         super(PositionalEncoding, self).__init__()
 
-        self.token_embedding = nn.Embedding(num_embeddings=52000, embedding_dim=dim_model, padding_idx=1, device=device)   # num_embeddings : vocab size
+        self.token_embedding = nn.Embedding(num_embeddings=52000, embedding_dim=dim_model, padding_idx=1)   # num_embeddings : vocab size
+        self.pos_encoding = torch.zeros(max_len, dim_model)
 
-        self.pos_encoding = torch.zeros(max_len, dim_model, device=device)
-
-        pos = torch.arange(0, max_len, dtype=torch.float, device=device).view(-1, 1)  # pos = tensor([0, 1, 2, 3, 4, 5]) -> (max_len, 1)의 2차원 tensor : 2차원이면서 두번째 차원은 1인 tensor 
+        pos = torch.arange(0, max_len, dtype=torch.float).view(-1, 1)  # pos = tensor([0, 1, 2, 3, 4, 5]) -> (max_len, 1)의 2차원 tensor : 2차원이면서 두번째 차원은 1인 tensor 
         
-        _2i = torch.arange(0, dim_model, step=2, device=device).float()
+        _2i = torch.arange(0, dim_model, step=2).float()
 
         self.pos_encoding[:, 0::2] = torch.sin(pos/(10000**(_2i/dim_model)))
         self.pos_encoding[:, 1::2] = torch.cos(pos/(10000**(_2i/dim_model)))
@@ -20,15 +19,16 @@ class PositionalEncoding(nn.Module):
         #self.register_buffer("pos_encoding", self.pos_encoding)   # it ensures that these tensors are treated properly 
                                                             # when the model is moved to different devices (CPU/GPU) and during model serialization.
 
-    def forward(self) -> torch.tensor:    # token_embedding을 torch.tensor로 받고, output 또한 torch.tensor로 내보냄
-        seq_len = self.token_embedding.size()
+    def forward(self, token_embedding:torch.tensor) -> torch.tensor:    # token_embedding을 torch.tensor로 받고, output 또한 torch.tensor로 내보냄
+        token_embedding = self.token_embedding(token_embedding)
+        seq_len = token_embedding.size(1)   # torch.Size([64, 63, 512])
 
         # token_embedding + pos encoding
-        return self.token_embedding + self.pos_encoding[:seq_len, :]
+        return token_embedding + self.pos_encoding[:seq_len, :]
 
 
 class MultiHeadAttention(nn.Module):    
-    def __init__(self, dim_model, num_heads, device):
+    def __init__(self, dim_model, num_heads):
         super(MultiHeadAttention, self).__init__()  
 
         assert dim_model % num_heads == 0       # dim_model=512, num_heads=8
@@ -93,31 +93,31 @@ class MultiHeadAttention(nn.Module):
         return output
 
 
-class FeedForward(nn.Module):       
-    def __init__(self, dim_model, hidden, device):
-        super(FeedForward, self).__init__() 
+class PositionwiseFeedForward(nn.Module):       # fully connected
+    def __init__(self, dim_model, hidden):      # dim_model=512, hidden=2048
+        super(PositionwiseFeedForward, self).__init__() 
         self.w1_linear = nn.Linear(dim_model, hidden)
         self.w2_linear = nn.Linear(hidden, dim_model)
 
     def forward(self, x):       # follow the FFN(x) = max(0, x*W1 + b1)W2 + b2
-         x_FFN = self.w1_linear(x)
-         x_FFN = nn.ReLU(x_FFN)
-         x_FFN = self.w2_linear(x_FFN)
+        x_FFN = self.w1_linear(x)
+        x_FFN = nn.ReLU(x_FFN)
+        x_FFN = self.w2_linear(x_FFN)
 
-         return x_FFN
+        return x_FFN
 
 
 class EncoderLayer(nn.Module):      # 6 layers
-    def __init__(self, dim_model, ffn_hidden, num_heads, device):
+    def __init__(self, dim_model, ffn_hidden, num_heads):
         super(EncoderLayer, self).__init__()
         self.dim_model = dim_model
         self.ffn_hidden = ffn_hidden
         self.num_heads = num_heads
 
-        self.multihead_attention = MultiHeadAttention(self.dim_model, self.num_heads, device=device)
+        self.multihead_attention = MultiHeadAttention(self.dim_model, self.num_heads)
         self.normalization1 = nn.LayerNorm(dim_model)
 
-        self.feedforward = FeedForward(self.dim_model, self.ffn_hidden, device=device)
+        self.feedforward = PositionwiseFeedForward(self.dim_model, self.ffn_hidden)
         self.normalization2 = nn.LayerNorm(self.dim_model)
     
     def forward(self, inputs):  #src_mask
@@ -130,16 +130,16 @@ class EncoderLayer(nn.Module):      # 6 layers
         return outputs
 
 class Encoder(nn.Module):   # num_layers = 6
-    def __init__(self, enc_voca_size, max_len, dim_model, ffn_hidden, num_heads, num_layers, device):
+    def __init__(self, enc_voca_size, max_len, dim_model, ffn_hidden, num_heads, num_layers):
         super(Encoder, self).__init__()
 
-        # self.embedding = nn.Embedding()
-        self.pos_encoding = PositionalEncoding(dim_model=dim_model, max_len=max_len, device=device)
+        # embedding : positional encoding에서 한 번에 처리함
+
+        self.pos_encoding = PositionalEncoding(dim_model=dim_model, max_len=max_len)
         # multi layers
         self.multi_layers = nn.ModuleList([EncoderLayer(dim_model=dim_model,
                                             ffn_hidden=ffn_hidden,
-                                            num_heads=num_heads,
-                                            device=device) for _ in range(num_layers)])
+                                            num_heads=num_heads) for _ in range(num_layers)])
     
     def forward(self, inputs):    #src_mask
         # embedding + pos_encoding
@@ -152,19 +152,19 @@ class Encoder(nn.Module):   # num_layers = 6
         return outputs
 
 class DecoderLayer(nn.Module):      # 6 layers
-    def __init__(self, dim_model, ffn_hidden, num_heads, device):
+    def __init__(self, dim_model, ffn_hidden, num_heads):
         super(DecoderLayer, self).__init__()
         self.dim_model = dim_model
         self.ffn_hidden = ffn_hidden
         self.num_heads = num_heads
 
-        self.masked_self_attention = MultiHeadAttention(self.dim_model, self.num_heads, device=device)   # masked 필요
+        self.masked_self_attention = MultiHeadAttention(self.dim_model, self.num_heads)   # masked 필요
         self.normalization1 = nn.LayerNorm(dim_model)
 
-        self.enc_dec_attention = MultiHeadAttention(self.dim_model, self.num_heads, device=device)
+        self.enc_dec_attention = MultiHeadAttention(self.dim_model, self.num_heads)
         self.normalization2 = nn.LayerNorm(dim_model)
 
-        self.feedforward = FeedForward(self.dim_model, ffn_hidden, device=device)
+        self.feedforward = PositionwiseFeedForward(self.dim_model, ffn_hidden)
         self.normalization3 = nn.LayerNorm(self.dim_model)
     
     def forward(self, enc_inputs, dec_inputs):      # trg_mask, src_mask
@@ -184,16 +184,15 @@ class DecoderLayer(nn.Module):      # 6 layers
         return outputs
     
 class Decoder(nn.Module):   # num_layers = 6
-    def __init__(self, dec_voca_size, max_len, dim_model, ffn_hidden, num_heads, num_layers, device):
+    def __init__(self, dec_voca_size, max_len, dim_model, ffn_hidden, num_heads, num_layers):
         super(Decoder, self).__init__()
 
         # self.embedding = nn.Embedding()
-        self.pos_encoding = PositionalEncoding(dim_model=dim_model, max_len=max_len, device=device)
+        self.pos_encoding = PositionalEncoding(dim_model=dim_model, max_len=max_len)
         # multi layers
         self.multi_layers = nn.ModuleList([DecoderLayer(dim_model=dim_model,
                                             ffn_hidden=ffn_hidden,
-                                            num_heads=num_heads,
-                                            device=device) for _ in range(num_layers)])
+                                            num_heads=num_heads) for _ in range(num_layers)])
         self.linear = nn.Linear(dim_model, dec_voca_size)
     def forward(self, src, trg):  #trg_mask, src_mask
         # embedding + pos_encoding
@@ -209,7 +208,7 @@ class Decoder(nn.Module):   # num_layers = 6
         return outputs
 
 class Transformer(nn.Module):
-    def __init__(self, dim_model, num_heads, max_len, ffn_hidden, num_layers, enc_voca_size, dec_voca_size, device):
+    def __init__(self, dim_model, num_heads, max_len, ffn_hidden, num_layers, enc_voca_size, dec_voca_size):
         super(Transformer, self).__init__()
     
         # Encoder
@@ -218,8 +217,7 @@ class Transformer(nn.Module):
                         dim_model = dim_model,
                         ffn_hidden = ffn_hidden, 
                         num_heads = num_heads, 
-                        num_layers = num_layers,
-                        device=device)
+                        num_layers = num_layers)
         
         # Decoder
         self.decoder = Decoder(dec_voca_size = dec_voca_size,
@@ -227,13 +225,10 @@ class Transformer(nn.Module):
                         dim_model = dim_model,
                         ffn_hidden = ffn_hidden, 
                         num_heads = num_heads, 
-                        num_layers = num_layers,
-                        device=device)
+                        num_layers = num_layers)
         
     def forward(self, src, trg):
         enc_src = self.encoder(src)
         output = self.decoder(trg, enc_src)
 
         return output
-
-    
